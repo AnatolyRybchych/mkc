194584fa0e972966c9e23438a1ce6c577f9b5a8a
AnatolyRybchych <tol.ryb@gmail.com>
2025-05-24 10:02:27 +0000

    using files to declare the structs and typedefs

--- mkc/file.py
+++ mkc/file.py
@@ -1,6 +1,31 @@
 
 from mkc.struct import Struct
+from mkc.typedef import Typedef
 
 class File:
     def __init__(self, path: str):
         self.path = path
+        self.typedefs = []
+        self.structs = []
+
+    def declare(self, target):
+        # TODO: check duplicate declarations
+
+        if type(target) is Struct:
+            self.structs.append(target)
+        elif type(target) is Typedef:
+            self.typedefs.append(target)
+        else:
+            raise Exception(f'the costruction of type {type(target)} is not ment to be declared')
+
+    def __str__(self):
+        # TODO: reorder accrding to dependencies
+        res = []
+
+        for typedef in self.typedefs:
+            res += [f'{typedef};']
+
+        for struct in self.structs:
+            res += [f'{struct};']
+
+        return '\n'.join(res)
--- run.py
+++ run.py
@@ -6,11 +6,11 @@ codebase = c.Codebase()
 main = codebase.add_new_file('main.c')
 
 point = c.Struct('Point')
+main.declare(point)
 
 point.add_field(c.int, 'x')
 point.add_field(c.int, 'y')
 
-point_t = c.Typedef(point, 'Point')
+main.declare(c.Typedef(point, 'Point'))
 
-print(point_t, ';')
-print(point, ';')
+print(main)
